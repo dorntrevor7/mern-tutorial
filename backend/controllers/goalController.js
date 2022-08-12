21ff65b6bc2d4ad507d49859fc30979e1d41fca4
dorntrevor7 <dorntrevor7@gmail.com>
2022-08-11 22:02:18 -0700

    routes, middleware, goalControllers set up

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require("excpress-async-handler");
+
+const getGoals = asyncHandler(async (req, res) => {
+  await res.status(200).json({ message: "Get goals" });
+});
+
+const postGoals = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+  await res.status(200).json({ message: "Set goals" });
+});
+
+const updateGoals = asyncHandler(async (req, res) => {
+  await res.status(200).json({ message: `Upate goal ${req.params.id}` });
+});
+
+const deleteGoals = asyncHandler(async (req, res) => {
+  await res.status(200).json({ message: `Delate goal ${req.params.id}` });
+});
+
+module.exports = {
+  getGoals,
+  postGoals,
+  updateGoals,
+  deleteGoals,
+};
---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getGoals,
+  postGoals,
+  updateGoals,
+  deleteGoals,
+} = require("../controllers/goalController");
+
+router.route("/").get(getGoals).post(postGoals);
+router.route("/:id").put(updateGoals).delete(deleteGoals);
+
+module.exports = router;
